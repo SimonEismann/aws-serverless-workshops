ffc5a9e77b61e3aa20d5a5e95adc8271c0e3a1b8
SimonEismann <31125121+SimonEismann@users.noreply.github.com>
2020-12-17 12:56:04 +0100

    Update run.sh

--- ImageProcessing/run.sh
+++ ImageProcessing/run.sh
@@ -23,7 +23,7 @@ APIURL=$(aws cloudformation describe-stacks --stack-name wildrydes --query "Stac
 STATEMACHINEARN=$(aws cloudformation describe-stacks --stack-name wildrydes --query "Stacks[0].Outputs[7].OutputValue" --output text)
 BUCKETNAME=$(aws cloudformation describe-stacks --stack-name wildrydes --query "Stacks[0].Outputs[8].OutputValue" --output text)
 
-curl -X POST -d '{"input": "{\"userId\": \"user_a\", \"s3Bucket\":\"$(BUCKETNAME)\", \"s3Key\": \"1_happy_face.jpg\"}", "stateMachineArn": "$(STATEMACHINEARN)"}' $APIURL
+curl -X POST -d '{"input": "{\"userId\": \"user_a\", \"s3Bucket\":\"${BUCKETNAME}\", \"s3Key\": \"1_happy_face.jpg\"}", "stateMachineArn": "${STATEMACHINEARN}"}' $APIURL
 
 # Wait
 sleep 3
