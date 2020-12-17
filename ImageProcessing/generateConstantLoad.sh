dd6bf6785a5b8f6b1e2892b9f6464a3565126334
SimonEismann <31125121+SimonEismann@users.noreply.github.com>
2020-12-17 20:52:48 +0100

    Update generateConstantLoad.sh

--- ImageProcessing/generateConstantLoad.sh
+++ ImageProcessing/generateConstantLoad.sh
@@ -2,8 +2,8 @@ intensity=$1
 durationInSeconds=$2
 rm -f load.csv
 echo "LOAD"
-echo durationInSeconds
-for ((n=1;n<=durationInSeconds;n++))
+echo $durationInSeconds
+for ((n=1;n<=$durationInSeconds;n++))
 do
 	timestamp=$((n - 1)).5
 	printf "$timestamp,$intensity\n" >> load.csv
