75d53264beaea5acd3b1391174e34490a54c9d58
SimonEismann <31125121+SimonEismann@users.noreply.github.com>
2020-12-18 11:39:29 +0100

    Update run.sh

--- ImageProcessing/run.sh
+++ ImageProcessing/run.sh
@@ -32,9 +32,9 @@ sed -i "s@BUCKETPLACEHOLDER@$BUCKETNAME@g" load.lua
 # Run Load
 java -jar httploadgenerator.jar loadgenerator > loadlogs.txt 2>&1 &
 chmod 777 generateConstantLoad.sh
-./generateConstantLoad.sh 10 180
+./generateConstantLoad.sh 10 1
 sleep 10
-java -jar httploadgenerator.jar director --ip localhost --load load.csv -o results.csv --lua load.lua --randomize-users -t 128
+java -jar httploadgenerator.jar director --ip localhost --load load.csv -o results.csv --lua load.lua --randomize-users -t 12
 
 # Shutdown
 aws cloudformation delete-stack --stack-name wildrydes
