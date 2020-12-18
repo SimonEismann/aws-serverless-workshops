0fa08edc9a9cf66d7f795eb98e29ed809e9bde00
SimonEismann <31125121+SimonEismann@users.noreply.github.com>
2020-12-18 21:35:24 +0100

    Update index.js

--- ImageProcessing/src/lambda-functions/thumbnail/index.js
+++ ImageProcessing/src/lambda-functions/thumbnail/index.js
@@ -4,6 +4,7 @@ const gm = require('gm').subClass({imageMagick: true}); // Enable ImageMagick in
 const util = require('util');
 const Promise = require('bluebird');
 Promise.promisifyAll(gm.prototype);
+const { v4: uuidv4 } = require('uuid');
 
 // constants
 const MAX_WIDTH = process.env.MAX_WIDTH ? process.env.MAX_WIDTH : 250;
@@ -14,7 +15,7 @@ const thumbnailBucket = process.env.THUMBNAIL_BUCKET;
 // get reference to S3 client
 const s3 = new AWS.S3();
 
-exports.handler = (event, context, callback) => {
+const lambdaHandler = (event, context, callback) => {
     console.log("Reading input from event:\n", util.inspect(event, {depth: 5}));
 
     // get the object from S3 first
@@ -100,3 +101,137 @@ exports.handler = (event, context, callback) => {
         callback(err);
     })
 }
+
+// monitoring function wrapping arbitrary payload code
+function handler(event, context, payload, callback) {
+  const child_process = require("child_process");
+  const v8 = require("v8");
+  const { performance, PerformanceObserver, monitorEventLoopDelay } = require("perf_hooks");
+  const [beforeBytesRx, beforePkgsRx, beforeBytesTx, beforePkgsTx] =
+    child_process.execSync("cat /proc/net/dev | grep vinternal_1| awk '{print $2,$3,$10,$11}'").toString().split(" ");
+  const startUsage = process.cpuUsage();
+  const beforeResourceUsage = process.resourceUsage();
+  const wrapped = performance.timerify(payload);
+  const h = monitorEventLoopDelay();
+  h.enable();
+
+  const durationStart = process.hrtime();
+
+  const ret = wrapped(event, context, callback);
+  h.disable();
+
+  const durationDiff = process.hrtime(durationStart);
+  const duration = (durationDiff[0] * 1e9 + durationDiff[1]) / 1e6
+
+  // Process CPU Diff
+  const cpuUsageDiff = process.cpuUsage(startUsage);
+  // Process Resources
+  const afterResourceUsage = process.resourceUsage();
+
+  // Memory
+  const heapCodeStats = v8.getHeapCodeStatistics();
+  const heapStats = v8.getHeapStatistics();
+  const heapInfo = process.memoryUsage();
+
+  // Network
+  const [afterBytesRx, afterPkgsRx, afterBytesTx, afterPkgsTx] =
+    child_process.execSync("cat /proc/net/dev | grep vinternal_1| awk '{print $2,$3,$10,$11}'").toString().split(" ");
+
+  const dynamodb = new AWS.DynamoDB({ region: 'eu-west-1' });
+  if (!event.warmup)
+    dynamodb.putItem({
+      Item: {
+        "id": {
+          S: uuidv4()
+        },
+        "duration": {
+          N: `${duration}`
+        },
+        "maxRss": {
+          N: `${afterResourceUsage.maxRSS - beforeResourceUsage.maxRSS}`
+        },
+        "fsRead": {
+          N: `${afterResourceUsage.fsRead - beforeResourceUsage.fsRead}`
+        },
+        "fsWrite": {
+          N: `${afterResourceUsage.fsWrite - beforeResourceUsage.fsWrite}`
+        },
+        "vContextSwitches": {
+          N: `${afterResourceUsage.voluntaryContextSwitches - beforeResourceUsage.voluntaryContextSwitches}`
+        },
+        "ivContextSwitches": {
+          N: `${afterResourceUsage.involuntaryContextSwitches - beforeResourceUsage.involuntaryContextSwitches}`
+        },
+        "userDiff": {
+          N: `${cpuUsageDiff.user}`
+        },
+        "sysDiff": {
+          N: `${cpuUsageDiff.system}`
+        },
+        "rss": {
+          N: `${heapInfo.rss}`
+        },
+        "heapTotal": {
+          N: `${heapInfo.heapTotal}`
+        },
+        "heapUsed": {
+          N: `${heapInfo.heapUsed}`
+        },
+        "external": {
+          N: `${heapInfo.external}`
+        },
+        "elMin": {
+          N: `${h.min}`
+        },
+        "elMax": {
+          N: `${h.max}`
+        },
+        "elMean": {
+          N: `${isNaN(h.mean) ? 0 : h.mean}`
+        },
+        "elStd": {
+          N: `${isNaN(h.stddev) ? 0 : h.stddev}`
+        },
+        "bytecodeMetadataSize": {
+          N: `${heapCodeStats.bytecode_and_metadata_size}`
+        },
+        "heapPhysical": {
+          N: `${heapStats.total_physical_size}`
+        },
+        "heapAvailable": {
+          N: `${heapStats.total_available_size}`
+        },
+        "heapLimit": {
+          N: `${heapStats.heap_size_limit}`
+        },
+        "mallocMem": {
+          N: `${heapStats.malloced_memory}`
+        },
+        "netByRx": {
+          N: `${afterBytesRx - beforeBytesRx}`
+        },
+        "netPkgRx": {
+          N: `${afterPkgsRx - beforePkgsRx}`
+        },
+        "netByTx": {
+          N: `${afterBytesTx - beforeBytesTx}`
+        },
+        "netPkgTx": {
+          N: `${afterPkgsTx - beforePkgsTx}`
+        }
+      },
+      TableName: "ThumbnailMetrics"
+    }, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.Item);
+  }
+});
+
+  return ret;
+};
+
+exports.handler = function(event, context, callback) {
+  return handler(event, context, lambdaHandler, callback);
+} 
