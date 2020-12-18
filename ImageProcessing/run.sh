6b61f4cb19e9cb9ca6782248e9338cc1721411fe
SimonEismann <31125121+SimonEismann@users.noreply.github.com>
2020-12-18 11:41:24 +0100

    Update run.sh

--- ImageProcessing/run.sh
+++ ImageProcessing/run.sh
@@ -35,6 +35,7 @@ chmod 777 generateConstantLoad.sh
 ./generateConstantLoad.sh 10 1
 sleep 10
 java -jar httploadgenerator.jar director --ip localhost --load load.csv -o results.csv --lua load.lua --randomize-users -t 12
+sleep 60
 
 # Shutdown
 aws cloudformation delete-stack --stack-name wildrydes
